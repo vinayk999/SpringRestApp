7fc84cc3fd9cd60ceac9f12457a40a227c68fccd
user <user@user-PC>
2018-11-13 09:54:16 +0530

    1001 First Commit,moving all files

---
+++ src/com/app/Demo3.java
@@ -0,0 +1,16 @@
+package com.app;
+
+public class Demo3 {
+
+	private int i=10;
+	public String name="vinay";
+	public static void main(String[] args) {
+		System.out.println("in Demo3");
+		
+		System.out.println("ok");
+		
+		System.out.println("bye");
+		
+		
+	}
+}
